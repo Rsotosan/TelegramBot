edcb1bcb05d23b1de21cfb84498e3948fa8db42b
Raúl Soto <sotosanchezraul@gmail.com>
2018-03-29 03:47:45 +0200

    WIP

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 function init(){
     const express = require('express');
     const app = express();
+    const bodyparser = require('body-parser');
+    var info = [];
+    app.use(bodyparser.json(), bodyparser.urlencoded({extended:true}));
+    app.post('/', async function(req, res){
+        info.push(req.body);
+        return res.sendStatus(200);
+    });
     app.get('/', async function(req, res){
-        return res.json({ 'test': 'test'});
+        return res.json(info);
     });
     app.listen(8080);
     return app;
