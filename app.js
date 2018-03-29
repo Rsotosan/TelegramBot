0f79f0c0a7b3b69bcefec758d892f951a780aa96
Raúl Soto <sotosanchezraul@gmail.com>
2018-03-29 04:09:21 +0200

    WIP

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ function init(){
     const express = require('express');
     const app = express();
     const bodyparser = require('body-parser');
+    var request = require('request');
     var info = [];
     app.use(bodyparser.json(), bodyparser.urlencoded({extended:true}));
     app.post('/', async function(req, res){
+        console.log('Recibido mensaje: ' + req.body);
         info.push(req.body);
+        request({
+            url: 'https://api.telegram.org/bot400962350:AAF-CQuT9w759Rib92mcs2Nhlj30R3hQntw/sendMessage',
+            method: 'POST',
+            json: {
+                chat_id: req.body.message.chat.id,
+                text: `Dijiste: ${req.body.message.text}`,
+                reply_to_message_id: req.body.message.message_id
+            }
+        });
         return res.sendStatus(200);
     });
     app.get('/', async function(req, res){
