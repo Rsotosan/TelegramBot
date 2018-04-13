b31a36d8d67b1bea63b5d7be148689ad3ee125db
Raúl Soto <sotosanchezraul@gmail.com>
2018-04-13 21:20:14 +0200

    WIP

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ function init(){
     app.post('/', async function(req, res){
         console.log('Recibido mensaje: ' + req.body);
         info.push(req.body);
+        var text =  `Dijiste: ${req.body.message.text}`;
+        if(req.body.message.text.toLowerCase('hola')){
+            text = 'Hola que tal :D';
+        }
         request({
             url: `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`,
             method: 'POST',
             json: {
                 chat_id: req.body.message.chat.id,
-                text: `Dijiste: ${req.body.message.text}`,
+                text,
                 reply_to_message_id: req.body.message.message_id
             }
         });
@@ -23,6 +27,7 @@ function init(){
         return res.json(info);
     });
     app.listen(8080);
+    console.log('SI');
     return app;
 }
 
