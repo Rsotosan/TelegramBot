58075215f5354574f250e98907322133da7ca0e8
Raúl Soto <sotosanchezraul@gmail.com>
2018-05-06 01:32:30 +0200

    wip

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
+const request = require('request');
+const express = require('express');
 function init(){
-    const express = require('express');
     const app = express();
     const bodyparser = require('body-parser');
-    var request = require('request');
-    var info = [];
     app.use(bodyparser.json(), bodyparser.urlencoded({extended:true}));
     app.post('/', async function(req, res){
-        console.log('Recibido mensaje: ' + req.body);
-        info.push(req.body);
-        var text =  `Dijiste: ${req.body.message.text}`;
-        if(req.body.message.text.toLowerCase().indexOf('hola') > -1){
-            text = 'Hola que tal :D';
-        }
-        request({
-            url: `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`,
-            method: 'POST',
-            json: {
-                chat_id: req.body.message.chat.id,
-                text,
-                reply_to_message_id: req.body.message.message_id
+        if(req.body.message){
+            var name = req.body.message.from.first_name;
+            var text =  `Hola amigo, ` + name + ' me gustaría saber cual es el animal más maravilloso del mundo, podrías ayudarme?';
+            if(req.body.message.text.toLowerCase().indexOf('flamenco') > -1){
+                text = 'Aqui iría el enlace al siguiente grupo';
             }
-        });
+            sendMessage(text, req.body.message.chat.id, req.body.message.message_id);
+        }
+        if(req.body.new_chat_members){
+            var name = req.body.new_chat_members.first_name;
+            var text =  `Hola amigo, ` + name + ' me gustaría saber cual es el animal más maravilloso del mundo, podrías ayudarme?';
+            sendMessage(text, req.body.message.chat.id);
+        }
         return res.sendStatus(200);
     });
     app.get('/', async function(req, res){
         return res.json(info);
     });
     app.listen(8080);
-    console.log('SI');
     return app;
+}
+
+var sendMessage = function(text, chat_id, reply){
+    var json = {
+        chat_id,
+        text}
+    if(reply){
+        json.reply_to_message_id = reply;
+    }
+    console.log(json);
+    console.log(process.env.TOKEN);
+    request({
+        url: `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`,
+        method: 'POST',
+        json
+    });
 }
 
 const app = init();
