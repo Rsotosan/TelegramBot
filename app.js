2bcf3b4f5b0cea6f9988aaabef00fdba034cd04b
Raúl Soto <sotosanchezraul@gmail.com>
2018-05-06 01:45:45 +0200

    wip

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ function init(){
     const app = express();
     const bodyparser = require('body-parser');
     app.use(bodyparser.json(), bodyparser.urlencoded({extended:true}));
+    var info = [];
     app.post('/', async function(req, res){
-        if(req.body.message){
+        info.push(req);
+        info.push('-----');
+        if(req.body.new_chat_members){
+            var name = req.body.new_chat_members[0].first_name;
+            var text =  `Hola amigo ` + name + ' ,me gustaría saber cual es el animal más maravilloso del mundo, podrías ayudarme?';
+            sendMessage(text, req.body.message.chat.id);
+        }
+        if(req.body.message && !req.body.new_chat_members){
             var name = req.body.message.from.first_name;
-            var text =  `Ay amigo, ` + name + ' creo que te has equivocado de animal o no se de que me estás hablando. ¿Podrías volver a intentarlo?';
+            var text =  `Ay amigo ` + name + ' ,creo que te has equivocado de animal o no se de que me estás hablando. ¿Podrías volver a intentarlo?';
             if(req.body.message.text && req.body.message.text.toLowerCase().indexOf('flamenco') > -1){
                 text = 'Aqui iría el enlace al siguiente grupo';
             }
             sendMessage(text, req.body.message.chat.id, req.body.message.message_id);
         }
-        if(req.body.new_chat_members){
-            var name = req.body.new_chat_members.first_name;
-            var text =  `Hola amigo ` + name + ' ,me gustaría saber cual es el animal más maravilloso del mundo, podrías ayudarme?';
-            sendMessage(text, req.body.message.chat.id);
-        }
+        
         return res.sendStatus(200);
     });
     app.get('/', async function(req, res){
