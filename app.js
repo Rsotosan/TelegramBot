f4519924cde9a65b0cfbfbc21925994a72276c53
Raúl Soto <sotosanchezraul@gmail.com>
2018-05-06 12:58:44 +0200

    wip

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const express = require('express');
+require('dotenv').config();
 function init(){
     const app = express();
     const bodyparser = require('body-parser');
@@ -25,11 +26,11 @@ function init(){
         if(!stop){
             if(req.body.message && !req.body.new_chat_members){
                 if(req.body.message.text && req.body.message.text.toLowerCase().indexOf('/frase') > -1){
-                    text = req.body.message.text.replace('/frase ', '');
+                    text = req.body.message.text.replace('/frase ', '');    
                     return res.sendStatus(200);
                 }
             }
-            if(req.body.message && !req.body.new_chat_members){
+            if(req.body.message && req.body.message.text){
                 var name = req.body.message.from.first_name;
                 if(req.body.message.text && req.body.message.text.toLowerCase().indexOf('flamenco') > -1){
                     text = 'Aqui iría el enlace al siguiente grupo';
@@ -55,6 +56,19 @@ var sendMessage = function(text, chat_id, reply){
     }
     request({
         url: `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`,
+        method: 'POST',
+        json
+    }).on('response', function(response){
+        sendSticker('CAADBAADSAADU9S8BcnOFkhyJiQfAg', chat_id);
+    });;
+}
+
+var sendSticker = function(sticker_id, chat_id){
+    var json = {
+        chat_id,
+        sticker: sticker_id}
+    request({
+        url: `https://api.telegram.org/bot${process.env.TOKEN}/sendSticker`,
         method: 'POST',
         json
     });
